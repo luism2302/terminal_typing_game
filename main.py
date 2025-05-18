af56b45f33dcf5a5303d93a31a35b636eb2a91ab
Luis2302 <luis.urzua2302@gmail.com>
2025-05-18 00:31:57 -0600

    Figuring out how to build a word based on input, have to take into account the backspace

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import sys
 import time
 import pygame
+import random
+
+all_words = []
+with open("words.txt", "r") as f:
+    for line in f:
+        all_words.append(line.strip())
+
 
 def main():
     pygame.init()
@@ -10,39 +17,58 @@ def main():
     font = pygame.font.Font(None, 40)
     #Welcome message
     welcome_txt = font.render("Welcome to my typing game", True, (255,255,255))
-    welcome_pos = welcome_txt.get_rect(centerx = screen.get_width()/2, y = 20)
+    welcome_pos = welcome_txt.get_rect(x = 10, y = 20)
     #Start the game input
-    start_txt = font.render("(Y) Start (N) Quit", True ,(255,255,255))
-    start_pos = welcome_txt.get_rect(centerx = screen.get_width()/2, y = 100)
+    start_y = font.render("(Y) Start", True ,(255,255,255))
+    start_y_pos = start_y.get_rect(x = 10, y = 80)
+    start_n = font.render("(N) Quit", True ,(255,255,255))
+    start_n_pos = start_n.get_rect(x = 10, y = 120)
+    #Variables for the different stages of the game
     stage = 0
+    countdown_nums = [3,2,1]
+    start_countdown = None
+    chosen_word = None
+    typed_word = ""
 
     while running:
         screen.fill("black")
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.TEXTINPUT and stage == 0:
-                match event.text:
-                    case "y":
-                        stage += 1
-                    case "n":
-                        running = False
+            print(event)
+            match event.type:
+                case pygame.QUIT:
+                    running = False
+                case pygame.TEXTINPUT:
+                    if stage == 0:
+                        match event.text:
+                            case "y":
+                                stage += 1
+                                start_countdown = pygame.time.get_ticks()
+                            case "n":
+                                running = False
+                    if stage == 2:
+                        pass
         if stage == 0: 
             screen.blit(welcome_txt, welcome_pos)
-            screen.blit(start_txt,start_pos)
+            screen.blit(start_y, start_y_pos)
+            screen.blit(start_n, start_n_pos)
+
         elif stage == 1:
-            get_ready_txt1 = font.render("Get ready! The first word will appear in ...3", True ,(255,255,255))
-            get_ready_txt2 = font.render("...2", True ,(255,255,255))
-            get_ready_txt3 = font.render("...3", True ,(255,255,255))
-            get_ready_txt1_pos = welcome_txt.get_rect(centerx = screen.get_width()/2, y = 100)
-            get_ready_txt2_pos = welcome_txt.get_rect(centerx = screen.get_width()/2, y = 120)
-            get_ready_txt3_pos = welcome_txt.get_rect(centerx = screen.get_width()/2, y = 140)
-            screen.blit(get_ready_txt1 , get_ready_txt1_pos)
-            pygame.time.delay(1000)
-            screen.blit(get_ready_txt2 , get_ready_txt2_pos)
-            pygame.time.delay(1000)
-            screen.blit(get_ready_txt3 , get_ready_txt3_pos)
+            elapsed = (pygame.time.get_ticks() - start_countdown) // 1000 
+            if elapsed < len(countdown_nums):
+                if elapsed == 0:
+                    get_ready_txt1 = font.render(f"Get ready! The first word will appear in ...{countdown_nums[elapsed]}", True ,(255,255,255))
+                else:
+                    get_ready_txt1 = font.render(f"...{countdown_nums[elapsed]}", True ,(255,255,255))
+                get_ready_txt1_pos = welcome_txt.get_rect(x = 10, y = elapsed)
+                screen.blit(get_ready_txt1 , get_ready_txt1_pos)
+            else:
+                stage += 1
+        elif stage == 2:
+            if chosen_word is None or typed_word == chosen_word:
+                chosen_word = random.choice(all_words)
 
         pygame.display.flip()
+        clock.tick(60)
+
 if __name__ == "__main__":
     main()
